72d7ec257a4075de30ff2fa50d87b37ae92988c9
Nuri-Kim <allmonde1994@gmail.com>
2022-12-13 08:48:44 +0900

    채팅 기능 구현

--- app/src/main/java/com/example/planter/Chat/ChatActivity.kt
+++ app/src/main/java/com/example/planter/Chat/ChatActivity.kt
@@ -2,7 +2,18 @@ package com.example.planter.Chat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fullstackapplication.ChatVO
+import com.example.fullstackapplication.utils.FBAuth
+import com.example.fullstackapplication.utils.FBdataBase
 import com.example.planter.R
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ktx.getValue
 
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +21,55 @@ class ChatActivity : AppCompatActivity() {
         setContentView(R.layout.activity_chat)
 
         // 실질적 채팅하는 창
+
+        val etChatInput = findViewById<EditText>(R.id.etChatInput)
+        val imgChatSend = findViewById<ImageView>(R.id.imgChatSend)
+        val rvChat = findViewById<RecyclerView>(R.id.rvChat)
+        
+        val chatList = ArrayList<ChatVO>()
+
+        val chatListRef = FBdataBase.getChatListRef()
+        val nowTime = FBAuth.getTime()
+
+        val adapter = ChatRoomAdapter(this,chatList,"receiver")
+        rvChat.adapter=adapter
+        rvChat.layoutManager=LinearLayoutManager(this)
+
+        imgChatSend.setOnClickListener{
+            val msg = etChatInput.text.toString()
+
+            chatListRef.push().setValue(ChatVO("receiver","sender",msg,"",nowTime))
+            etChatInput.text = null
+        }
+        
+        chatListRef.addChildEventListener(object : ChildEventListener{
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val chatItem = snapshot.getValue<ChatVO> () as ChatVO
+                
+                chatList.add(chatItem)
+                
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/planter/Chat/ChatListAdapter.kt
@@ -0,0 +1,87 @@
+package com.example.planter.Chat
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fullstackapplication.ChatVO
+import com.example.planter.R
+
+
+    // 현재 로그인 유저 전체 채팅 목록
+class ChatListAdapter (val context: Context,
+                       val chatList : ArrayList<ChatVO>,
+                       val loginId : String) : RecyclerView.Adapter<ChatListAdapter.ViewHolder>() {
+
+
+        // 없는 기능을 인터페이스를 생성하여 커스텀하여 사용
+        // 리스너 커스텀
+        interface OnItemClickListener{
+            fun onItemClick(view : View, position: Int)
+        }
+
+        // 객체 저장 변수 선언
+        lateinit var mOnItemClickListener:OnItemClickListener
+
+        // 객체 전달 메서드
+        fun setOnItemClickListener(onItemClickListener: OnItemClickListener){
+            mOnItemClickListener = onItemClickListener
+        }
+
+
+
+    inner class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        val imgChatList : ImageView
+        val tvChatListOther : TextView
+        val tvChatListContent : TextView
+        val tvChatTime : TextView
+
+        init {
+            imgChatList=itemView.findViewById(R.id.imgChatList)
+            tvChatListOther=itemView.findViewById(R.id.tvChatListOther)
+            tvChatListContent=itemView.findViewById(R.id.tvChatListContent)
+            tvChatTime = itemView.findViewById(R.id.tvChatTime)
+
+
+            // 채팅 리스트 내 행 클릭 시 채팅창으로 이동
+            itemView.setOnClickListener{
+                val position = adapterPosition
+                if(position != RecyclerView.NO_POSITION){
+                    mOnItemClickListener.onItemClick(itemView,position)
+                }
+            }
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(context)
+        val view = layoutInflater.inflate(R.layout.chat_list_template,null)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val recUser = chatList[position].recUser
+
+        // fireBase chatList 내 receiver 가 login user 와 일치하는 경우
+        // 같은 발신자가 보낸 메세제 통합 작업 필요
+        // 다음 position 발신자가 이미 리스트에 있다면 데이터 대체하기
+
+        if( "receiver" == recUser){
+            holder.imgChatList.setImageResource(R.drawable.icon_home)
+            holder.tvChatListOther.text = chatList[position].sendUser
+            holder.tvChatListContent.text = chatList[position].msg
+            holder.tvChatTime.text = chatList[position].time
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return chatList.size
+    }
+}
