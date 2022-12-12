489fe194f3e29bdda35c82288fb1b3c2c94a3ee5
zindol2 <yangjunho444@gmail.com>
2022-12-12 14:15:15 +0900

    양준호

---
+++ app/src/main/java/com/example/planter/Join/JoinActivity.kt
@@ -0,0 +1,20 @@
+package com.example.planter.Join
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.planter.R
+import org.w3c.dom.Text
+
+class JoinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/planter/Login/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.planter.Login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.example.planter.MainActivity
+import com.example.planter.R
+
+class LoginActivity : AppCompatActivity() {
+
+    //lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        //auth = Firebase.auth
+
+        val etLoginEmail = findViewById<TextView>(R.id.etLoginEmail)
+        val etLoginPw = findViewById<TextView>(R.id.etLoginPw)
+        val btnLoginLogin = findViewById<Button>(R.id.btnLoginLogin)
+
+        btnLoginLogin.setOnClickListener {
+            val email = etLoginEmail.text.toString()
+            val pw = etLoginPw.text.toString()
+
+            val intent = Intent(this, MainActivity::class.java)
+
+
+
+        }
+
+
+    }
+}
