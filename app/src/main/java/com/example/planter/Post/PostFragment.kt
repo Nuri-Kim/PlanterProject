58f65900fbc053d2bf4502bcc04e41067cf923d5
ipjyie <iusp0054@gamil.com>
2022-12-12 16:20:41 +0900

    사진 및 xml파일

--- app/src/main/java/com/example/planter/Post/PostFragment.kt
+++ app/src/main/java/com/example/planter/Post/PostFragment.kt
@@ -1,14 +1,20 @@
 package com.example.planter.Post
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.planter.R
 
 
+
 class PostFragment : Fragment() {
+
 
 
 
@@ -16,8 +22,17 @@ class PostFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_post, container, false)
+
+
+        val rvPostView = view.findViewById<RecyclerView>(R.id.rvPostView)
+
+        val PostList = ArrayList<PostVO>()
+
+
+        return view
+
 
 
     }
