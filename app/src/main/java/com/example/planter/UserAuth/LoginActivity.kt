7173cff882161e87012dcded003b2be0cfa154dc
zindol2 <yangjunho444@gmail.com>
2022-12-12 15:23:14 +0900

    양준호

--- app/src/main/java/com/example/planter/UserAuth/LoginActivity.kt
+++ app/src/main/java/com/example/planter/UserAuth/LoginActivity.kt
@@ -1,10 +1,12 @@
 package com.example.planter.UserAuth
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import com.example.planter.MainActivity
 import com.example.planter.R
 import com.google.firebase.auth.FirebaseAuth
@@ -18,22 +20,45 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         //auth = Firebase.auth
+        val sharedPreferences = getSharedPreferences("autoLogin", Context.MODE_PRIVATE)
+        val loginId = sharedPreferences.getString("loginId","")
+        val loginPw = sharedPreferences.getString("loginPw","")
 
         val etLoginEmail = findViewById<TextView>(R.id.etLoginEmail)
         val etLoginPw = findViewById<TextView>(R.id.etLoginPw)
         val btnLoginLogin = findViewById<Button>(R.id.btnLoginLogin)
 
-        val sp = getSharedPreferences()
+        val sp = getSharedPreferences("loginInfo", Context.MODE_PRIVATE)
+
 
         btnLoginLogin.setOnClickListener {
             val email = etLoginEmail.text.toString()
             val pw = etLoginPw.text.toString()
 
-            if(email){
+            auth.signInWithEmailAndPassword(email,pw)
+                .addOnCompleteListener(this){task ->
+
+                    if(task.isSuccessful){
+                        Toast.makeText(this," ${email}님 반가워요",Toast.LENGTH_SHORT).show()
+
+                        val editor = sharedPreferences.edit()
+                        editor.putString("loginId",email)
+                        editor.putString("loginPw",pw)
+                        editor.commit()
+
+                        val editorSp = sp.edit()
+                        editorSp.putString("loginId",email)
+                        editorSp.commit()
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+
+                    }
 
             }
 
-            val intent = Intent(this, MainActivity::class.java)
+
 
 
 
