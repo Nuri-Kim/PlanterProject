ad805988cca55ec1aa6529f6ffc6b5120c89ebb6
Nuri-Kim <allmonde1994@gmail.com>
2022-12-12 11:51:01 +0900

    package 생성

---
+++ app/src/main/java/com/example/planter/Chat/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.example.planter.Chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.planter.R
+
+class ChatActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
