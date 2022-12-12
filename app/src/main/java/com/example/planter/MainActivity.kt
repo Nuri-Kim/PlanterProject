32a8ff448bb9d900acbecd45745eceab27867789
Nuri-Kim <allmonde1994@gmail.com>
2022-12-12 15:07:35 +0900

    프래그먼트 연결

--- app/src/main/java/com/example/planter/MainActivity.kt
+++ app/src/main/java/com/example/planter/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.planter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import android.widget.ImageView
+import com.example.planter.Chat.ChatFragment
+import com.example.planter.Post.PostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imgLogo = findViewById<ImageView>(R.id.imgLogo)
+        val bnvMain = findViewById<BottomNavigationView>(R.id.bnvMain)
+        val flMain = findViewById<FrameLayout>(R.id.flMain)
+
+        bnvMain.setOnItemSelectedListener{
+            item ->
+            when(item.itemId){
+
+                R.id.tab2 ->{
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.flMain,
+                        PostFragment()
+                    ).commit()
+                }
+
+                R.id.tab3 ->{
+                    supportFragmentManager.beginTransaction().replace(
+                        R.id.flMain,
+                        ChatFragment()
+                    ).commit()
+                }
+
+            }
+
+        true
+
+        }
     }
 }
