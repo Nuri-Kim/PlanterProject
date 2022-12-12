248163c68c880d96747d02276f2e70529bdccc6b
m1otic <parkminook16@naver.com>
2022-12-12 11:44:53 +0900

    package 생성(민욱/)

---
+++ app/src/main/java/com/example/planter/Api/ApiActivity.kt
@@ -0,0 +1,12 @@
+package com.example.planter.Api
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.planter.R
+
+class ApiActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_api)
+    }
+}
---
+++ app/src/main/java/com/example/planter/Push/PushActivity.kt
@@ -0,0 +1,12 @@
+package com.example.planter.Push
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.planter.R
+
+class PushActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_push)
+    }
+}
