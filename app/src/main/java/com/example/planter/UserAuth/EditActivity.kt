7fc8e1a5ea2374fccbecdb8fd892171ca39023ff
zindol2 <yangjunho444@gmail.com>
2022-12-13 16:39:18 +0900

    회원정보수정페이지 만드는 중

---
+++ app/src/main/java/com/example/planter/UserAuth/EditActivity.kt
@@ -0,0 +1,17 @@
+package com.example.planter.UserAuth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.planter.R
+
+class EditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/planter/UserAuth/IntroActivity.kt
+++ app/src/main/java/com/example/planter/UserAuth/IntroActivity.kt
@@ -25,38 +25,77 @@ class IntroActivity : AppCompatActivity() {
         setContentView(R.layout.activity_intro)
 
         val btnIntroLogin = findViewById<Button>(R.id.btnIntroLogin)
+
         val btnIntroJoin = findViewById<Button>(R.id.btnIntroJoin)
+
         val btnIntroLogOut = findViewById<Button>(R.id.btnIntroLogOut)
+
         val btnIntroDel = findViewById<Button>(R.id.btnIntroDel)
+
+        val btnIntroEdit = findViewById<Button>(R.id.btnIntroEdit)
 
         auth = Firebase.auth
 
+
+
+
+
+        // 로그인 버튼
         btnIntroLogin.setOnClickListener{
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
 
+
+
+
+
+        // 회원가입 버튼
         btnIntroJoin.setOnClickListener{
             val intent = Intent(this,JoinActivity::class.java)
             startActivity(intent)
         }
 
+
+
+
+
+        // 로그아웃 버튼
         btnIntroLogOut.setOnClickListener {
 
             FirebaseAuth.getInstance().signOut()
             Toast.makeText(this,"로그아웃하셨습니다",Toast.LENGTH_SHORT).show()
 
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+
+
+        // 회원탈퇴 버튼
+        btnIntroDel.setOnClickListener {
+
+            auth.currentUser?.delete()
+            FirebaseAuth.getInstance().signOut()
+            Toast.makeText(this,"탈퇴 성공",Toast.LENGTH_SHORT).show()
             val intent = Intent(this,MainActivity::class.java)
-                startActivity(intent)
-            }
+            startActivity(intent)
+        }
 
-            btnIntroDel.setOnClickListener {
 
-                auth.currentUser?.delete()
-                Toast.makeText(this,"탈퇴 성공",Toast.LENGTH_SHORT).show()
-                val intent = Intent(this,MainActivity::class.java)
-                startActivity(intent)
-            }
+
+
+
+        // 회원정보 수정
+        btnIntroEdit.setOnClickListener {
+
+
+
+
+
 
         }
     }
+}
