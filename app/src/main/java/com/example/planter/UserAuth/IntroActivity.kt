6f9d8dbe837b7b39d9da989dff39cf739a595daa
zindol2 <yangjunho444@gmail.com>
2022-12-13 15:28:36 +0900

    회원가입 로그인

--- app/src/main/java/com/example/planter/UserAuth/IntroActivity.kt
+++ app/src/main/java/com/example/planter/UserAuth/IntroActivity.kt
@@ -44,18 +44,14 @@ class IntroActivity : AppCompatActivity() {
         btnIntroLogOut.setOnClickListener {
 
             FirebaseAuth.getInstance().signOut()
-                Toast.makeText(this,"로그아웃하셨습니다",Toast.LENGTH_SHORT).show()
-            Log.d("현재 유저","로그아웃 테스트임 ${FBAuth.getUid()}")
+            Toast.makeText(this,"로그아웃하셨습니다",Toast.LENGTH_SHORT).show()
 
             val intent = Intent(this,MainActivity::class.java)
                 startActivity(intent)
-
             }
 
             btnIntroDel.setOnClickListener {
-//            val sp = getSharedPreferences("loginInfo", Context.MODE_PRIVATE)
-//            val editorSp = sp.edit()
-//            val email  = editorSp.getString("loginId","아이디")
+
                 auth.currentUser?.delete()
                 Toast.makeText(this,"탈퇴 성공",Toast.LENGTH_SHORT).show()
                 val intent = Intent(this,MainActivity::class.java)
