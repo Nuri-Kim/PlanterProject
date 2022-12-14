c383bb679db3a0714c927f20d01e7a19b2c3efff
zindol2 <yangjunho444@gmail.com>
2022-12-14 12:39:20 +0900

    마이페이지 버튼 기능 연결

--- app/src/main/java/com/example/planter/MyPage/MyPageFragment.kt
+++ app/src/main/java/com/example/planter/MyPage/MyPageFragment.kt
@@ -7,10 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import com.example.fullstackapplication.utils.FBAuth.Companion.auth
+import androidx.appcompat.app.AlertDialog
 import com.example.planter.MainActivity
 import com.example.planter.R
+import com.example.planter.UserAuth.EditActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -30,20 +32,60 @@ class MyPageFragment : Fragment() {
 
         val view = inflater.inflate(R.layout.fragment_my_page, container, false)
         val btnMyPageLogOut = view.findViewById<Button>(R.id.btnMyPageLogOut)
-        val btnMyPageDel = view.findViewById<Button>(R.id.btnMyPageDel)
+        val tvMyPageDel = view.findViewById<TextView>(R.id.tvMyPageDel)
+        val btnMyPageEdit = view.findViewById<Button>(R.id.btnMyPageEdit)
+
+
+
+        btnMyPageEdit.setOnClickListener {
+
+            val intent = Intent(context,EditActivity::class.java)
+        }
 
 
 
         btnMyPageLogOut.setOnClickListener {
             auth.signOut()
-            Toast.makeText(this,"로그아웃 하셨습니다", Toast.LENGTH_SHORT).show()
+            Toast.makeText(context,"로그아웃 하셨습니다", Toast.LENGTH_SHORT).show()
 
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(context, MainActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
         }
 
-        btnMyPageDel.setOnClickListener {
+        tvMyPageDel.setOnClickListener {
+
+            // AlertDialog 옵션 정보를 담고 builder 생성!
+            // AlertDialog.Builder(this).setTitle("asd").setMessage("asdf")
+            // 이런 식으로 한 줄에 쓸 수도 있음
+            val builder = AlertDialog.Builder(requireContext())
+
+            builder.setIcon(R.drawable.dotol)
+            builder.setTitle("도토리")
+            builder.setMessage("정말 탈퇴할거야?")
+
+//            builder.setPositiveButton("탈퇴", DialogInterface.OnClickListener() {
+//                @Override
+//                public void onClick(DialogInterface dialog, int which) {
+//                    Toast.makeText(getApplicationContext(), "Yeah!!", Toast.LENGTH_LONG).show();
+//                }
+            builder.setPositiveButton("탈퇴하기"){dialogInterface, it ->
+
+
+                auth.currentUser?.delete()
+                FirebaseAuth.getInstance().signOut()
+                Toast.makeText(context, "탈퇴 성공", Toast.LENGTH_SHORT).show()
+                val intent = Intent(context, MainActivity::class.java)
+                startActivity(intent)
+
+            }
+            builder.setNegativeButton("취소", null)
+
+            // 주의
+            // builder를 통해 옵션을 단 이후
+            // 맨 마지막에는 무조건!! show()함수를 달아야 한다
+
+            builder.show()
 
 
         }
