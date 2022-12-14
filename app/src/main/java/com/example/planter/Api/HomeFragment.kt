3561ca8a10591b4de3ddc42cd54ca0d0859b7f71
m1otic <parkminook16@naver.com>
2022-12-14 09:06:12 +0900

    박민욱 pull하기전

--- app/src/main/java/com/example/planter/Api/HomeFragment.kt
+++ app/src/main/java/com/example/planter/Api/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.ListView
+import android.widget.Switch
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,7 +16,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.example.fullstackapplication.ChatVO
+import com.example.fullstackapplication.utils.FBAuth
+import com.example.fullstackapplication.utils.FBAuth.Companion.auth
+import com.example.fullstackapplication.utils.FBdataBase
 import com.example.planter.R
+import com.example.planter.UserAuth.JoinVO
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import org.json.JSONObject
 import java.math.BigInteger
 import java.text.SimpleDateFormat
@@ -31,12 +42,14 @@ class HomeFragment : Fragment() {
 
     val sdf = SimpleDateFormat("HH시 mm분")
 
+    val database = Firebase.database
+    val user = FBAuth.getUid()
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         //현재시간(ㅁ밀리)
         val currentMillis = LocalDateTime.now()
             .atZone(ZoneId.systemDefault())
@@ -49,11 +62,39 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
         val imgHomeReset = view.findViewById<ImageView>(R.id.imgHomeReset)
-
         val lvHome = view.findViewById<ListView>(R.id.lvHome)
         val rvHome = view.findViewById<RecyclerView>(R.id.rvHome)
+        val swHomeAlarm1 = view.findViewById<Switch>(R.id.swHomeAlarm1)
+        val swHomeAlarm2 = view.findViewById<Switch>(R.id.swHomeAlarm2)
 
+        val list = ArrayList<JoinVO>()
         val weatherList = ArrayList<WeatherVO>()
+
+        var uidRef = database.getReference("JoinList")
+
+                for(i in 0 until 1){
+            //push() key 타임스탬프를 찍어줌 (고유한 값을 만들어줌)
+            uidRef.push().setValue(
+                list[i]
+            )
+        }
+
+        swHomeAlarm1.setOnCheckedChangeListener { compoundButton, ischecked ->
+            if(ischecked){
+                if(uidRef.orderByChild("email").equals(auth.currentUser!!.email)){
+
+                }
+            }else{
+
+            }
+        }
+        swHomeAlarm2.setOnCheckedChangeListener { compoundButton, ischecked ->
+            if(ischecked){
+
+            }else{
+
+            }
+        }
 
         //Volley 활용한 네트워크 통신
         //1. volley 라이브러리 설정, 인터넷 권한 허용
@@ -118,5 +159,29 @@ class HomeFragment : Fragment() {
 
         return view
     }
+
+    //저장되어 있는 child값 전부 한번 돌고 조건을 줘서 이메일이 같으면 해당 세팅값만 변경하면됨
+//    fun setSettings(ck:Boolean){
+//        val postListener = object : ValueEventListener {
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                chatList.clear()
+//                for(model in snapshot.children){
+//                    val item = model.getValue(ChatVO::class.java)
+//                    if(item != null){
+//                        chatList.add(item)
+//                    }
+//                    keyData.add(model.key.toString())
+//                }
+//                adapter.notifyDataSetChanged()
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                TODO("Not yet implemented")
+//            }
+//
+//        }
+//
+//        FBdataBase.getChatListMyFilterRef(user).addValueEventListener(postListener)
+//    }
 
 }
